0ba5dec50d6052ef7e4a80d92b2e8ebacd27d822
Elina Vagapova <vagapovaelina98@gmail.com>
2022-07-31 19:34:36 +0300

    lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+const screenPrice = 12;
+const rollback = 5;
+const fullPrice = 250000;
+let adaptive;
+
+alert("Hello JavaScript!");
+
+console.log("Мое первое домашнее задание");
