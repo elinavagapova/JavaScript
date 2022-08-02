675f3967e4e457dfe85b537acdc0664b77702475
Elina Vagapova <vagapovaelina98@gmail.com>
2022-08-02 21:28:20 +0300

    Выполненное дз2

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let title;
-let screens;
+let title = "lesson02";
+let screens = "Простые, Сложные, Интерактивные";
 const screenPrice = 12;
 const rollback = 5;
 const fullPrice = 250000;
-let adaptive;
+let adaptive = 2 > 1;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase());
+console.log(screens.split(","));
+
+console.log(fullPrice * rollback/100 + "%");
 
 alert("Hello JavaScript!");
 
